15049a05b96f2f10953c87a04a870b700dfefa75
amora7741 <mora7741@gmail.com>
2024-01-25 12:34:55 -0800

    Add logo to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,22 @@
 import renderSearchBar from './search';
+import cloud from '../icons/weather-cloudy.svg';
 
 export default function renderHeader() {
   const header = document.createElement('header');
+  const logo = document.createElement('div');
+  logo.className = 'logo';
+
+  const icon = new Image();
+  icon.src = cloud;
+
+  logo.appendChild(icon);
+
   const title = document.createElement('h1');
   title.textContent = 'BreezyZone';
 
-  header.appendChild(title);
+  logo.appendChild(title);
+
+  header.appendChild(logo);
   header.appendChild(renderSearchBar());
 
   return header;
