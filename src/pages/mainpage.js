712d07f83de9768aa9c233d81eb0f67bbd6e6d9f
amora7741 <mora7741@gmail.com>
2024-01-25 14:12:50 -0800

    Fetch 3 day forecast

--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -5,7 +5,9 @@ export default function renderMainPage(container) {
 
   const searchbtn = document.querySelector('#search');
   searchbtn.addEventListener('click', (e) => {
-    getData();
+    getData().then(function (data) {
+      console.log(data);
+    });
     e.preventDefault();
   });
 }
@@ -15,11 +17,11 @@ async function getData() {
   const qry = document.querySelector('#cityinput').value;
 
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${qry}`,
+    `https://api.weatherapi.com/v1/forecast.json?key=${KEY}&q=${qry}&days=3`,
     { mode: 'cors' }
   );
 
   const weatherData = await response.json();
 
-  console.log(weatherData);
+  return weatherData;
 }
